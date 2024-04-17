1bdc9ece5c22b6e5a158b6db734725e79d86b987
youngseojang <youngseojang@gmail.com>
2024-04-17 19:12:56 +0900

    feature: 상태바 투명하게 적용

--- app/src/main/java/com/example/cammate/MainActivity.kt
+++ app/src/main/java/com/example/cammate/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setStatusBarTransparent()
         initToolbar()
         initTabLayout()
     }
