e6750e1706fa603d7f8902270dcbe5ae10f9f8e5
youngseojang <youngseojang@gmail.com>
2024-04-18 04:11:41 +0900

    feature: fragmentContainer를 담는 프래그먼트 생성

---
+++ app/src/main/java/com/example/cammate/ContainerFragment.kt
@@ -0,0 +1,29 @@
+package com.example.cammate
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import com.example.cammate.databinding.FragmentContainerBinding
+
+class ContainerFragment : Fragment() {
+    private var _binding: FragmentContainerBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentContainerBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d("tag", "컨테이너")
+    }
+}
