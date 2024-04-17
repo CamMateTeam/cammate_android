956c771a216afba1c6c6f3dbf2a307b080c682c5
youngseojang <youngseojang@gmail.com>
2024-04-18 04:16:24 +0900

    feature: nav graph 이용한 프래그먼트 전환 구현

--- app/src/main/java/com/example/cammate/FindRoomFragment.kt
+++ app/src/main/java/com/example/cammate/FindRoomFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cammate
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +9,10 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cammate.databinding.FragmentFindRoomBinding
+import com.example.cammate.find_room.adapter.CammatesAdapter
+import com.example.cammate.find_room.adapter.CammatesItem
 
 class FindRoomFragment : Fragment() {
-
     private var _binding: FragmentFindRoomBinding? = null
     private val binding get() = _binding!!
     private val cammates = listOf(
@@ -41,7 +43,11 @@ class FindRoomFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnFindRoomNext.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            try {
+                findNavController().navigate(R.id.action_SecondFragment_to_enterRoomFragment)
+            } catch (e: Exception){
+                Log.d("tag", "에러가 났굼나유")
+            }
         }
     }
 
