8b338d17e0f9b2e1336c1a474959677150708c38
youngseojang <youngseojang@gmail.com>
2024-04-24 02:43:17 +0900

    feature: 방만들기 플로우에 대한 nav graph

--- app/src/main/java/com/example/cammate/make_room/MakeRoomFragment.kt
+++ app/src/main/java/com/example/cammate/make_room/MakeRoomFragment.kt
@@ -1,10 +1,13 @@
 package com.example.cammate.make_room
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.cammate.R
 import com.example.cammate.databinding.FragmentMakeRoomBinding
 
 class MakeRoomFragment : Fragment() {
@@ -15,13 +18,21 @@ class MakeRoomFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMakeRoomBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            try {
+                findNavController().navigate(R.id.action_makeRoomFragment_to_waitingRoomFragment)
+            } catch (e: Exception){
+                Log.d("tag", "에러가 났굼나유")
+            }
+        }
     }
 
     override fun onDestroyView() {
