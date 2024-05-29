478c3c223f1ae11a9b8f670cfcda98d0b004c591
youngseojang <youngseojang@gmail.com>
2024-05-29 17:04:29 +0900

    feature: 메인액티비티에서 블루투스 지원되는지 확인

--- app/src/main/java/com/example/cammate/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cammate/presentation/MainActivity.kt
@@ -5,11 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import androidx.activity.result.ActivityResultLauncher
 import androidx.annotation.RequiresApi
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.viewpager2.widget.ViewPager2
@@ -19,38 +17,51 @@ import com.example.cammate.utils.setStatusBarTransparent
 import com.example.cammate.presentation.find_room.FindRoomHostFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.snackbar.Snackbar
 
-
-@RequiresApi(Build.VERSION_CODES.M)
 class MainActivity : AppCompatActivity() {
-    private val REQUEST_CODE_ENABLE_BT: Int = 1
+    lateinit var bluetoothManager: BluetoothManager
+    lateinit var bluetoothAdapter: BluetoothAdapter
+
+    private val TAG = "MainActivity"
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var bluetoothManager: BluetoothManager
-    private lateinit var bluetoothAdapter: BluetoothAdapter
-
-    // Activity Result Launcher
-    private lateinit var activityResultLauncherBluetooth: ActivityResultLauncher<Intent>
-
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val splashScreen = installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        installSplashScreen()
         setStatusBarTransparent()
+
+        // 블루투스어댑터 초기화
+        bluetoothManager = getSystemService(BluetoothManager::class.java)
+        bluetoothAdapter = bluetoothManager.adapter
+
+        // 블루투스 지원되는지 확인
+        checkSupportBT()
+
         initToolbar()
         initTabLayout()
     }
 
-    fun initToolbar() {
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun checkSupportBT() {
+        if (bluetoothAdapter == null){
+            showToast("Device doesn't support Bluetooth")
+            finish() /// 지원하지 않으면 종료
+        }
+    }
+
+    private fun initToolbar() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         binding.toolbar.title = "cammate"
     }
 
-    fun initTabLayout() {
+    private fun initTabLayout() {
         val tabTextList = listOf("방 만들기", "방 찾기")
         val tabLayout: TabLayout = binding.tabLayout
         val viewPager: ViewPager2 = binding.mainViewPager
@@ -82,6 +93,10 @@ class MainActivity : AppCompatActivity() {
             R.id.action_help -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun showToast(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
     }
 
 }
