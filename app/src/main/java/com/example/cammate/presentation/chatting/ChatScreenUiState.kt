a293d1a4f7d2654816c14818ada1b31f06bbda1a
hei-jung <heijung.jang@hotmail.com>
2024-06-07 14:28:28 +0900

    채팅 파일들 불필요한 거 삭제

--- app/src/main/java/com/example/cammate/presentation/chatting/ChatScreenUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cammate.presentation.chatting
-
-data class ChatScreenUiState(
-    var messages: List<ChatMessage> = listOf(),
-    val userId: String = "",
-    val message: String = "",
-    val connectionStatus: ConnectionStatus = ConnectionStatus.NOT_STARTED
-)
--- app/src/main/java/com/example/cammate/presentation/chatting/ConnectionStatus.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cammate.presentation.chatting
-
-enum class ConnectionStatus {
-    NOT_STARTED, OPENED, CLOSED, CONNECTING, CLOSING, FAILED, RECEIVED
-}
